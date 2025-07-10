70b9b0d83a297ca517a1261e0848cfb4f8464dbe
Azwer Sajeeth <140418925+amsajeeth@users.noreply.github.com>
2025-07-10 12:44:57 +0530

    Add files via upload

---
+++ Hand tracking module.py
@@ -0,0 +1,54 @@
+import cv2
+import mediapipe as mp
+import time
+
+from mediapipe.python.solutions import hands
+
+
+class HandDetector():
+    def __init__(self,mode=False,maxHands=2,detectionCon=0.5,trackCon=0.5):
+        self.mode = mode
+        self.maxHands = maxHands
+        self.detectionCon = detectionCon
+        self.trackCon = trackCon
+
+        self.mpHands = mp.solutions.hands
+        self.hands = self.mpHands.Hands(self.mode,self.maxHands,self.detectionConfidence,self.trackingConfidence)
+        self.mpDraw = mp.solutions.drawing_utils
+
+    def findHands(self, img, draw=True):
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        results = hands.process(imgRGB)
+        # print(results.multi_hand_landmarks)
+
+        if results.multi_hand_landmarks:
+            for handLms in results.multi_hand_landmarks:
+             if draw:
+                self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
+
+
+        return img
+
+
+
+
+
+
+def main():
+    pTime = 0
+    cTime = 0
+    cap = cv2.VideoCapture(0)
+    detector = HandDetector()
+    while True:
+        success, img = cap.read()
+        img= detector.findHands(img)
+
+        cTime = time.time()
+        fps: float = 1 / (cTime - pTime)
+        pTime = cTime
+
+        cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3,
+                        (255, 0, 255), 3)
+
+        cv2.imshow("Image", img)
+        cv2.waitKey(1)
